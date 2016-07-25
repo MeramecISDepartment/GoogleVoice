7f294cd8a9a34af4d794b6062caecee26a7875f4
Tyler Barton <tbarton33@outlook.com>
2016-07-25 14:39:14 -0500

    This is an attempt at adding a delete method...it didn't work.

--- Test2/Test3/Test4/GoogleVoice/GoogleVoice/Form1.cs
+++ Test2/Test3/Test4/GoogleVoice/GoogleVoice/Form1.cs
@@ -51,12 +51,26 @@ namespace GoogleVoice
             //http://stackoverflow.com/questions/8861854/read-a-file-from-an-unknown-location
             //System.IO.File.Delete(@"C:\Users\Tyler Barton\Documents\.credentials\calendar-dotnet-quickstart.json\Google.Apis.Auth.OAuth2.Responses.TokenResponse-user");
             credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
-            System.IO.File.Delete(@credPath);
+            //System.IO.File.Delete(@credPath);
+            //System.IO.File.Delete(@".credentials/calendar-dotnet-quickstart.json");
+            //System.IO.File.Delete(@"calendar-dotnet-quickstart.json");
+            //string extension = Path.GetExtension(Directory.GetFiles(NETWORK_FOLDER_PATH, "calendar-dotnet-quickstart.json")[0]);
+
+            /*
+             * I think that this might be the solution for deleting a file that's at a different location on each computer.
+             * http://stackoverflow.com/questions/1240373/how-do-i-get-the-current-username-in-net-using-c
+             * by jaun
+             */
+            //string userName = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+            string userName1 = System.Environment.UserName;
+            //string credentials = "C:/Users/" + userName + "/Documents/.credentials/calendar-dotnet-quickstart.json/Google.Apis.Auth.OAuth2.Responses.TokenResponse-user";
+            System.IO.File.Delete(@"C:/Users/" + userName1 + "/Documents/.credentials/calendar-dotnet-quickstart.json/Google.Apis.Auth.OAuth2.Responses.TokenResponse-user");
+
             //System.IO.File.Delete(@"Google.Apis.Auth.OAuth2.Responses.TokenResponse-user");
             //}
 
             Choices voiceCommands = new Choices();
-            voiceCommands.Add(new string[] { "Add an event to my Google Calendar."});
+            voiceCommands.Add(new string[] { "Add an event to my Google Calendar.", "Delete an event from my Google Calendar."});
             GrammarBuilder grammarBuilder = new GrammarBuilder();
             //grammarBuilder.AppendDictation(); //New
             grammarBuilder.Append(voiceCommands);
@@ -95,10 +109,11 @@ namespace GoogleVoice
                         newEvent.login();
                         synthesizer.SpeakAsync("Your credentials have been saved to " + credPath + ".");
                         synthesizer.SpeakAsync("You have logged in.  What would you like to name your event?");
-
+                        synthesizer.SpeakAsync("Did you say ...");
 
 
                         synthesizer.SpeakAsync("What description would you like to give your event?");
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         //synthesizer.SpeakAsync("When does your event begin?");
 
@@ -107,36 +122,58 @@ namespace GoogleVoice
                         //if there is no input or an error in input, the user should be prompted to fill the field out properly
 
                         synthesizer.SpeakAsync("During what year does your event start?"); //perhaps look at whether the event month has already passed and if so go to the next year
+                        synthesizer.SpeakAsync("Did you say ...");
                         synthesizer.SpeakAsync("During what month does your event start?");
+                        synthesizer.SpeakAsync("Did you say ...");
                         synthesizer.SpeakAsync("During what day does your event start?");
+                        synthesizer.SpeakAsync("Did you say ...");
                         synthesizer.SpeakAsync("During what time does your event start and is it AM or PM??"); //Note, since this is military time 12:00AM - 11:59AM is 1:00 to 12:59 and 12:00PM to 11:59PM is 13:00 to 24:00
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         synthesizer.SpeakAsync("During what year does your event end?");  //perhaps look at whether the event month has already passed or is before the start month and go to the next year
+                        synthesizer.SpeakAsync("Did you say ...");
                         synthesizer.SpeakAsync("During what month does your event end");
+                        synthesizer.SpeakAsync("Did you say ...");
                         synthesizer.SpeakAsync("During what day does your event end?");
+                        synthesizer.SpeakAsync("Did you say ...");
                         synthesizer.SpeakAsync("During what time does your event end and is it AM or PM??");
+                        synthesizer.SpeakAsync("Did you say ...");
                         //synthesizer.SpeakAsync("When does your event end?");
 
 
                         synthesizer.SpeakAsync("In what time zone are you in?"); //perhaps get it off the computer clock
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         synthesizer.SpeakAsync("How often would you like for this event to repeat (never, daily, bi-daily, tri-daily, weekly, bi-weekly, tri-weekly, monthly, yearly)?");
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         synthesizer.SpeakAsync("Would you to add any attendees (yes or no)?");
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         synthesizer.SpeakAsync("Would is the attendee's email address?");
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         synthesizer.SpeakAsync("You you like to add another attendee (yes or no)?");
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         synthesizer.SpeakAsync("Would you like to add reminders (yes or no)?");
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         synthesizer.SpeakAsync("What type of reminder would you like to give (text or email)?"); //Note: When saved, text = sms
+                        synthesizer.SpeakAsync("Did you say ...");
 
                         synthesizer.SpeakAsync("How much time before the event would you like the reminder (days, hours, minutes, seconds)?"); //Note: 24 * 60 is 1 day
                         //synthesizer.SpeakAsync("How many minutes, hours, or days before the event would you like your reminder to be?");
+                        synthesizer.SpeakAsync("Did you say ...");
 
-                        synthesizer.SpeakAsync("");
+                        //synthesizer.SpeakAsync("");
                         newEvent.addNewEvent();
+                        synthesizer.SpeakAsync("Your new event has been added."); //Give a message with the different information.
+                        break;
+
+                    case "Delete an event from my Google Calendar.":
+                        deleteEvent delEvent = new deleteEvent();
+                        delEvent.deleteAnEvent();
                         break;
                 }
             });
--- Test2/Test3/Test4/GoogleVoice/GoogleVoice/deleteEvent.cs
+++ Test2/Test3/Test4/GoogleVoice/GoogleVoice/deleteEvent.cs
@@ -1,12 +1,83 @@
-﻿using System;
+﻿//Google Calendar related namespaces
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+//using Google.Apis;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading; //This is used in the voice to text application
+using System.Windows.Forms;
 namespace GoogleVoice
 {
     class deleteEvent
     {
+        public CalendarService service;
+
+        public void login()
+        {
+            string[] Scopes = { CalendarService.Scope.Calendar };
+            string ApplicationName = "Google Calendar API .NET Quickstart";
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                //synthesizer.SpeakAsync("Credential file saved to: " + credPath);
+                //richTextBox1.Text = "Credential file saved to: " + credPath;
+            }
+
+            // Create Google Calendar API service.
+            service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        public void deleteAnEvent()
+        {
+            //I referenced Google Developer and Linda Lawton for this:
+            //https://github.com/LindaLawton/Google-Dotnet-Samples/blob/master/Google-Calendar/Google-Calendar-Api-dotnet/Google-Clndr-Api-dotnet/Google-Clndr-Api-dotnet/DaimtoEventHelper.cs
+            // Initialize Calendar service with valid OAuth credentials
+            //Calendar service = new Calendar.Builder(httpTransport, jsonFactory, credentials)
+            //    .setApplicationName("applicationName").build();
+
+            // Delete an event
+            //service.Events.Delete("primary", "Google I/O 2015").deleteAnEvent.execute;
+            //service.events().delete('primary', "eventId").execute();
+            //I will reference this kind soul in the future:
+            //https://groups.google.com/forum/#!topic/google-calendar-api/gZO18rvvqHM
+
+            //try
+            //{
+             //   return service.Events.Delete('primary', 'Google I/O 2015').Execute();
+            //}
+            //catch (Exception ex)
+            //{
+                //Console.WriteLine(ex.Message);
+                //return null;
+            //}
+        }
     }
 }
