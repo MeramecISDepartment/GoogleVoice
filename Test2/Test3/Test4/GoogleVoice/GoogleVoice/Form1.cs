8a0fab03fd1b423e697ca82a7b37a7553788c26c
Tyler Barton <tbarton33@outlook.com>
2016-07-25 11:38:16 -0500

    I'm about to start another version of the project.

---
+++ Test2/Test3/Test4/GoogleVoice/GoogleVoice/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading; //This is used in the voice to text application
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//Speech recognition related namespaces
+using System.Speech; //This is used in the voice to text application
+using System.Speech.Recognition; //This is used in the voice to text application
+using System.Speech.Synthesis; //This is used in the voice to text application
+
+//Audio namespaces
+
+namespace GoogleVoice
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Thread RecThread; //New
+        public Boolean RecognizerState = true; //New
+
+        public string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //string credPath = System.Environment.GetFolderPath(
+        //                    System.Environment.SpecialFolder.Personal);
+
+        //Speech recognition related items
+        SpeechRecognitionEngine recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //if (credPath != System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal))
+            //{
+            //These web pages helped me to learn to delete files.
+            //https://msdn.microsoft.com/en-us/library/cc148994(v=vs.100).aspx
+            //http://stackoverflow.com/questions/8861854/read-a-file-from-an-unknown-location
+            //System.IO.File.Delete(@"C:\Users\Tyler Barton\Documents\.credentials\calendar-dotnet-quickstart.json\Google.Apis.Auth.OAuth2.Responses.TokenResponse-user");
+            credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+            System.IO.File.Delete(@credPath);
+            //System.IO.File.Delete(@"Google.Apis.Auth.OAuth2.Responses.TokenResponse-user");
+            //}
+
+            Choices voiceCommands = new Choices();
+            voiceCommands.Add(new string[] { "Add an event to my Google Calendar."});
+            GrammarBuilder grammarBuilder = new GrammarBuilder();
+            //grammarBuilder.AppendDictation(); //New
+            grammarBuilder.Append(voiceCommands);
+            Grammar grammar = new Grammar(grammarBuilder);
+
+            recognizer.LoadGrammarAsync(grammar);
+            recognizer.SetInputToDefaultAudioDevice();
+            //recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized); //New
+            recognizer.SpeechRecognized += recognizer_SpeechRecognized;
+
+            recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            //RecognizerState = true;
+            //RecThread = new Thread(new ThreadStart(RecThreadFunction)); //?
+            //RecThread.Start();
+
+            synthesizer.SpeakAsync("What would you like to do with Google Calendar.");
+        }
+
+
+        //Speech recognition related method
+        void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            if (!RecognizerState)
+                return;
+
+            this.Invoke((MethodInvoker)delegate
+            {
+                // richTextBox1.Text += ("" + e.Result.Text.ToLower());
+
+                switch (e.Result.Text)
+                {
+                    case "Add an event to my Google Calendar.":
+                        synthesizer.SpeakAsync("Please give your credentials first.");
+                        addEvent newEvent = new addEvent();
+                        newEvent.login();
+                        synthesizer.SpeakAsync("Your credentials have been saved to " + credPath + ".");
+                        synthesizer.SpeakAsync("You have logged in.  What would you like to name your event?");
+
+
+
+                        synthesizer.SpeakAsync("What description would you like to give your event?");
+
+                        //synthesizer.SpeakAsync("When does your event begin?");
+
+                        //word numbers will be converted to and saved as numbers using swithc cases //same with states to abbreviations to save
+                        //all inputs should be verified back to the user in words until the user says that they are okay
+                        //if there is no input or an error in input, the user should be prompted to fill the field out properly
+
+                        synthesizer.SpeakAsync("During what year does your event start?"); //perhaps look at whether the event month has already passed and if so go to the next year
+                        synthesizer.SpeakAsync("During what month does your event start?");
+                        synthesizer.SpeakAsync("During what day does your event start?");
+                        synthesizer.SpeakAsync("During what time does your event start and is it AM or PM??"); //Note, since this is military time 12:00AM - 11:59AM is 1:00 to 12:59 and 12:00PM to 11:59PM is 13:00 to 24:00
+
+                        synthesizer.SpeakAsync("During what year does your event end?");  //perhaps look at whether the event month has already passed or is before the start month and go to the next year
+                        synthesizer.SpeakAsync("During what month does your event end");
+                        synthesizer.SpeakAsync("During what day does your event end?");
+                        synthesizer.SpeakAsync("During what time does your event end and is it AM or PM??");
+                        //synthesizer.SpeakAsync("When does your event end?");
+
+
+                        synthesizer.SpeakAsync("In what time zone are you in?"); //perhaps get it off the computer clock
+
+                        synthesizer.SpeakAsync("How often would you like for this event to repeat (never, daily, bi-daily, tri-daily, weekly, bi-weekly, tri-weekly, monthly, yearly)?");
+
+                        synthesizer.SpeakAsync("Would you to add any attendees (yes or no)?");
+
+                        synthesizer.SpeakAsync("Would is the attendee's email address?");
+
+                        synthesizer.SpeakAsync("You you like to add another attendee (yes or no)?");
+
+                        synthesizer.SpeakAsync("Would you like to add reminders (yes or no)?");
+
+                        synthesizer.SpeakAsync("What type of reminder would you like to give (text or email)?"); //Note: When saved, text = sms
+
+                        synthesizer.SpeakAsync("How much time before the event would you like the reminder (days, hours, minutes, seconds)?"); //Note: 24 * 60 is 1 day
+                        //synthesizer.SpeakAsync("How many minutes, hours, or days before the event would you like your reminder to be?");
+
+                        synthesizer.SpeakAsync("");
+                        newEvent.addNewEvent();
+                        break;
+                }
+            });
+        }
+
+        public void RecThreadFunction()
+        {
+            while (true)
+            {
+                try
+                {
+                    recognizer.Recognize();
+                }
+                catch
+                {
+                   // synthesizer.SpeakAsync("I did not understand you.");
+                }
+            }
+        }
+    }
+}
---
+++ Test2/Test3/Test4/GoogleVoice/GoogleVoice/addEvent.cs
@@ -0,0 +1,142 @@
+﻿//Google Calendar related namespaces
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+//using Google.Apis;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Threading; //This is used in the voice to text application
+using System.Windows.Forms;
+
+namespace GoogleVoice
+{
+
+    class addEvent
+    {
+    
+        public CalendarService service;
+            
+        public void login()
+        {
+            string[] Scopes = { CalendarService.Scope.Calendar };
+            string ApplicationName = "Google Calendar API .NET Quickstart";
+
+            UserCredential credential;
+
+            using (var stream =
+                new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/calendar-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                //synthesizer.SpeakAsync("Credential file saved to: " + credPath);
+                //richTextBox1.Text = "Credential file saved to: " + credPath;
+            }
+
+            // Create Google Calendar API service.
+            service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+        }
+
+        public void addNewEvent()
+        {
+            
+
+            // Refer to the .NET quickstart on how to setup the environment:
+            // https://developers.google.com/google-apps/calendar/quickstart/dotnet
+            // Change the scope to CalendarService.Scope.Calendar and delete any stored
+            // credentials.
+            ;
+            Event newEvent = new Event()
+            {
+                Summary = "Google I/O 2015",
+                //prompt for summary, verify and store
+                Location = "800 Howard St., San Francisco, CA 94103",
+                //prompt for address
+                //prompt for city
+                //prompt for state
+                //prompt for zipcode
+                //make a string that says address + ", " + city + ", " + state + " " + zipcode
+                //have it so that if any state the initials will shop up
+                //verify with each one before saving.
+
+                Description = "A chance to hear more about Google's developer products.",
+                Start = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse("2015-05-28T09:00:00-07:00"),
+                    TimeZone = "America/Los_Angeles",
+                },
+                End = new EventDateTime()
+                {
+                    DateTime = DateTime.Parse("2015-05-28T17:00:00-07:00"),
+                    //prompt year and save it
+                    //prompt month and save it
+                    //prompt day and save it
+                    //prompt start hour and save it (12/12 instead of 24)
+                    //take extra at the end and save it to minutes, o'clock (00)
+                    //seconds at the end should be saved (none, 00 seconds)
+                    //prompt end time and save it 
+                    //take extra at the end and save it to minutes
+                    //take extra seconds at the end and save them
+                    //put a string together as year + "-" + month + "-" + day + "T" + hour1 + ":" + minutes1 + ":" + seconds "-" + hour2 + ":" + minutes2 + ":" + seconds2
+
+                    TimeZone = "America/Los_Angeles",
+                    //Have a list of time zones
+                },
+                Recurrence = new String[] { "RRULE:FREQ=DAILY;COUNT=2" },
+                //How frequent would you like for it to occur store the number
+                //once does not repeat the event
+                //daily/monthly will store the choice if not either will return an error 
+                //save a string as "RRULE:FREQ=" + interval + ";COUNT=" + frequency
+
+                Attendees = new EventAttendee[]
+                {
+                                 new EventAttendee() { Email = "lpage@example.com" },
+                                 //prompt for an email
+                                 //verify that it is in the proper format *****@***.*** (org, net, com, edu, etc.)
+                                 //verify that it exists and save/
+                                 //prompt for another
+                                 //etc 
+                                 //maybe save to a collection or a list
+                                 new EventAttendee() { Email = "sbrin@example.com" },
+                },
+                Reminders = new Event.RemindersData()
+                {
+                    UseDefault = false,
+                    Overrides = new EventReminder[]
+                    {
+                                  new EventReminder() { Method = "email", Minutes = 24 * 60 },
+                                  //prompt method (email, sms, other)
+                                  //prompt minutes
+                                  //prompt for (what 60 is)
+                                  new EventReminder() { Method = "sms", Minutes = 10 },
+                    }
+                }
+            };
+
+            String calendarId = "primary";
+            EventsResource.InsertRequest request = service.Events.Insert(newEvent, calendarId);
+            Event createdEvent = request.Execute();
+        }
+    }
+}
